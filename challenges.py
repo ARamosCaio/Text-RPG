579e29769dcacdc4824ef31813d7c7aab82cf9ae
Caio Vinícius Araújo Ramos <aramoscaio@gmail.com>
2023-03-05 19:12:32 -0300

    fix: update code structure/legibility

--- challenges.py
+++ challenges.py
@@ -1,10 +1,9 @@
 def riddle(answer):
-    if answer == "Buraco" or answer == "Um buraco" or answer == "buraco" or answer == "Um Buraco" or answer == 'BURACO':
-        result = '1'
-        return result
-    else:
-        result = '2'
-        return result
+    return (
+        '1'
+        if answer in ["Buraco", "Um buraco", "buraco", "Um Buraco", "BURACO"]
+        else '2'
+    )
     
 
 
