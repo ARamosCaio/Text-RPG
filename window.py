91396da3b11571c5102896bd57042f117e5e3457
Caio Ramos <aramoscaio@gmail.com>
2023-04-22 00:27:10 -0300

    feat: added tkinter gui

--- window.py
+++ window.py
@@ -1,3 +1,11 @@
-from tk import *
+from tkinter import *
 import tkinter as tk
 
+class App(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+
+root = tk.Tk()
+app = App(master=root)
+app.mainloop()
