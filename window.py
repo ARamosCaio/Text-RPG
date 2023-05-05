dd4edf62fb21f0905e01ba655b732d5911a6e916
Caio Ramos <aramoscaio@gmail.com>
2023-05-05 18:34:48 -0300

    feat: added second button

--- window.py
+++ window.py
@@ -7,14 +7,20 @@ root.geometry("800x800")
 root.title("Main")
 
 
-text = tk.Entry(root, width=77, background="black")
+text = tk.Entry(root, width=90)
 text.pack()
 
 def first_action():
     text.delete(0,"end")
     text.insert(0, "You Opened de door")
 
+def second_action():
+    text.delete(0,"end")
+    text.insert(0, "You enter the room")
+
 set_btn = tk.Button(root, text="Open the door", command=first_action, height=3, width=10)
 set_btn.pack()
-    
+
+set_btn2 = tk.Button(root, text="Enter the room", command=second_action, height=3, width=10)
+set_btn2.pack()
 root.mainloop()
