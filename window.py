9251928b5c7f3139fdfed1b06832705c2564a3d2
Caio Ramos <aramoscaio@gmail.com>
2023-05-05 18:24:32 -0300

    feat: increased text window width

--- window.py
+++ window.py
@@ -7,14 +7,14 @@ root.geometry("800x800")
 root.title("Main")
 
 
-text = tk.Entry(root)
+text = tk.Entry(root, width=77, background="black")
 text.pack()
 
 def first_action():
     text.delete(0,"end")
     text.insert(0, "You Opened de door")
 
-set_btn = tk.Button(root, text="Open the door", command=first_action)
+set_btn = tk.Button(root, text="Open the door", command=first_action, height=3, width=10)
 set_btn.pack()
     
 root.mainloop()
