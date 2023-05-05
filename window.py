0a682a64a71a164ef42da9805c89f094ccc67cc2
Caio Ramos <aramoscaio@gmail.com>
2023-05-05 18:19:37 -0300

    fix: corrected button bug

--- window.py
+++ window.py
@@ -2,11 +2,10 @@ from tkinter import *
 from tkinter import ttk
 import tkinter as tk
 
-root = Tk()
+root = tk.Tk()
 root.geometry("800x800")
 root.title("Main")
-screen = Entry(root, text="Testing...", width=78,  justify='right', font=10, bd=1)
-screen.grid(row=0, columnspan=1, padx=5, pady=30, ipady=30)
+
 
 text = tk.Entry(root)
 text.pack()
@@ -15,6 +14,7 @@ def first_action():
     text.delete(0,"end")
     text.insert(0, "You Opened de door")
 
-tk.Button(root, text="Open the door", command=first_action)
+set_btn = tk.Button(root, text="Open the door", command=first_action)
+set_btn.pack()
     
 root.mainloop()
