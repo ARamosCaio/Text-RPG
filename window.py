81466393ae76f1a06f496ef90d8270be770f0d24
Caio Ramos <aramoscaio@gmail.com>
2023-05-04 22:28:31 -0300

    feat: Added main window

--- window.py
+++ window.py
@@ -1,11 +1,7 @@
 from tkinter import *
 import tkinter as tk
 
-class App(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
+root = Tk()
+root.geometry("800x800")
 
-root = tk.Tk()
-app = App(master=root)
-app.mainloop()
+root.mainloop()
