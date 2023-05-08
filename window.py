6ae2d1498850c49097c99873dba0702a027474bb
Caio Ramos <aramoscaio@gmail.com>
2023-05-07 22:40:23 -0300

    feat: created text and stats frames

--- window.py
+++ window.py
@@ -6,12 +6,20 @@ class App():
     def __init__(self):
         self.root = root
         self.screen_config()
+        self.screen_frames()
         root.mainloop()
     
     def screen_config(self):
         self.root.title("ROOMS")
         self.root.geometry("1000x900")
-        self.root.configure(background="#121212")
+        self.root.configure(background="#dbdbdb")
         self.root.resizable(False, False)
+    
+    def screen_frames(self):
+        self.text_frame = Frame(self.root, bd=2, bg="#121212", highlightbackground="#5c5c5c", highlightthickness=4)
+        self.text_frame.place(relx=0.005, rely=0.005, relwidth=0.99, relheight=0.70)
+
+        self.stats_frame = Frame(self.root, bd=2, bg="#121212", highlightbackground="#5c5c5c", highlightthickness=4)
+        self.stats_frame.place(relx=0.005, rely=0.710, relwidth=0.4, relheight=0.28)
 
 App()
